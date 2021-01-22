6312603dcf92d44e68417130d85f443016758f42
Chukwuka1488 <juniorakibor@yahoo.com>
2021-01-22 23:11:46 +0100

    Derive age function

--- bot.py
+++ bot.py
@@ -6,3 +6,14 @@ def remind_name():
     print('Please, remind me your name.')
     name = input()
     print('What a great name you have, ' + name + '!')
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
